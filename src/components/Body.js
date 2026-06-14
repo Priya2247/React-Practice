aa9f91f946603c03687b718de0374fad8963e0af
Priyadharshini Sekar <priyasekar2247@gmail.com>
2026-06-14 23:58:37 +0530

    Feat : search button added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,13 +4,19 @@ import { useState } from "react";
 
 const Body = () =>{
     const [RestaurantList,setRestaurantList]=useState(resList);
+    const [searchText, setSearchText]=useState("");
     return(
         <> 
+          <div className="filters">
             <button className="filter-btn" onClick={()=>{
                 const filteredList=resList.filter((Restaurant)=> Restaurant.card.card.info.avgRating>4.5);
                 console.log(filteredList);
                 setRestaurantList(filteredList);
                 }}>Top Restaurants</button>
+            <button className="search-btn" value={searchText} onChange={(e)=>{
+                    setSearchText(e.target.value);
+                }}></button>
+            </div>
             <div className='card-container'>
                 {
                     RestaurantList.map((Restaurant) => 
